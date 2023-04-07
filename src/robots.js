d4727f7906818eaeec940a43042f8cdaf685fda7
vijay arun <blockloader78@gmail.com>
2023-04-07 15:57:07 +0530

    Create robots.js

---
+++ src/robots.js
@@ -0,0 +1,93 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Vijay Arun',
+    username: 'Bret',
+    email: 'Sincere@april.biz'
+  },
+  {
+    id: 2,
+    name: 'Abi',
+    username: 'Antonette',
+    email: 'Shanna@melissa.tv'
+  },
+  {
+    id: 3,
+    name: 'Harini',
+    username: 'Samantha',
+    email: 'Nathan@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Shyam',
+    username: 'Karianne',
+    email: 'Julianne.OConner@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Sairam',
+    username: 'Kamren',
+    email: 'Lucio_Hettinger@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Prem',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Arvind',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Hamshika',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Keerthana',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Aathi',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  },
+  {
+    id: 11,
+    name: 'Aswin',
+    username: 'Bret',
+    email: 'Sincere@april.biz'
+  },
+  {
+    id: 12,
+    name: 'Sakthi',
+    username: 'Bret',
+    email: 'Sincere@april.biz'
+  },
+  {
+    id: 13,
+    name: 'Kairav',
+    username: 'Bret',
+    email: 'Sincere@april.biz'
+  },
+  {
+    id: 14,
+    name: 'Jeevesh',
+    username: 'Bret',
+    email: 'Sincere@april.biz'
+  },
+  {
+    id: 15,
+    name: 'Divyesh',
+    username: 'Bret',
+    email: 'Sincere@april.biz'
+  },
+
+];
