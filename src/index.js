3aa4e5ca9e24cd4167157eb6eee4ed7cdb657957
vijay arun <blockloader78@gmail.com>
2023-04-07 15:57:02 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import 'tachyons';
+import Dad from './Father';
+ReactDOM.createRoot(
+  document.getElementById('root')
+  ).render(
+    <div>
+      <Dad/>
+    </div>
+);
+    
